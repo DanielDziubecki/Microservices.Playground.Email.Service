1e97a346e266532fc4d007945fdb92e6145fe942
Daniel <daniel.dziubecki@wp.pl>
2017-09-27 20:46:39 +0200

    migration to .net core

---
+++ Email.Service/Consumers/OrderSuccededConsumer.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+using MassTransit;
+using Shared.Payment.Contracts;
+
+namespace Email.Service.Consumers
+{
+    public class OrderSuccededConsumer : IConsumer<IPaymentProcessSucceded>
+    {
+        public Task Consume(ConsumeContext<IPaymentProcessSucceded> context)
+        {
+            //send email 
+            return Task.CompletedTask;
+        }
+    }
+}
